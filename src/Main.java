19d17328e1ce9f3433edc3201c3f0daaa80c1345
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-10-08 10:49:25 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Reverse Array Challenge!");
         int[] numberArray = {6,7,8,9,10,5,4,3,2,1};
-        System.out.println("Unreverse Array: " + Arrays.toString(numberArray));
+//        System.out.println("Unreverse Array: " + Arrays.toString(numberArray));
 //        System.out.println("Reverse Array: " + Arrays.toString(reverse(numberArray)));
-        reverse(numberArray);
-        System.out.println("Final: " + Arrays.toString(numberArray));
+//        reverse(numberArray);
+//        System.out.println("Final: " + Arrays.toString(numberArray));
+        int[] reverseCopy = reverseCopy(numberArray);
+        System.out.println("Before reverse " + Arrays.toString(numberArray));
+        System.out.println("After reverse " + Arrays.toString(reverseCopy));
     }
 
     private static void reverse(int[] array){
@@ -20,5 +23,15 @@ public class Main {
             array[maxIndex - i] = temp;
             System.out.println("--->" +  Arrays.toString(array));
         }
+    }
+
+    private static int[] reverseCopy(int[] array){
+        int[] reversedArray = new int[array.length];
+        int maxIndex = array.length - 1;
+        for(int el: array){
+            reversedArray[maxIndex--] = el;
+
+        }
+        return reversedArray;
     }
 }
