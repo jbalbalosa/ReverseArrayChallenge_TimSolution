12279d975a30b0168b96bb13fe79e042d645c7a2
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-10-08 10:35:24 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Reverse Array Challenge!");
+        int[] numberArray = {6,7,8,9,10,5,4,3,2,1};
+        System.out.println("Unreverse Array: " + Arrays.toString(numberArray));
+//        System.out.println("Reverse Array: " + Arrays.toString(reverse(numberArray)));
+        reverse(numberArray);
+        System.out.println("Final: " + Arrays.toString(numberArray));
+    }
+
+    private static void reverse(int[] array){
+      int maxIndex = array.length - 1;
+      int halfLength = array.length / 2;
+
+        for (int i = 0; i < halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+            System.out.println("--->" +  Arrays.toString(array));
+        }
     }
 }
